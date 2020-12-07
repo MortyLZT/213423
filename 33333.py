f4309326a471af1f65af1658bddda742ff250dc5
Morty <52600928+MortyLZT@users.noreply.github.com>
2020-12-07 12:19:59 +0200

    Update and rename 3333.py to 33333.py

--- 33333.py
+++ 33333.py
@@ -57,7 +57,7 @@ class JacquesThreeMod(loader.Module):
 		atwo = a / 2
 		full = 512 / 2
 		fds = full - atwo
-		imtext = Image.new("RGBA", (w+50, h+50), (0, 0,0,0))
+		imtext = Image.new("RGBA", (fds, h+50), (0, 0,0,0))
 		draw = ImageDraw.Draw(imtext)
 		draw.multiline_text((40, 40),t,(0,0,0),font=font, align='left')
 		imtext.thumbnail((450, 330))
