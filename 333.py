502f774e0ad2aa4633a4414f1c62265c49eeaa9d
Morty <52600928+MortyLZT@users.noreply.github.com>
2020-12-07 12:17:34 +0200

    Update and rename 33.py to 333.py

--- 333.py
+++ 333.py
@@ -59,7 +59,7 @@ class JacquesThreeMod(loader.Module):
 		draw = ImageDraw.Draw(imtext)
 		draw.multiline_text((40, 40),t,(0,0,0),font=font, align='left')
 		imtext.thumbnail((450, 330))
-		w, h = as, 330
+		w, h = 12, 330
 		img.paste(imtext, (2,100), imtext)
 		out = io.BytesIO()
 		out.name = "@sad0ff.webp"
