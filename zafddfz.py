0d2c64ec4db6c050006773fb25c3f47a94328eef
Morty <52600928+MortyLZT@users.noreply.github.com>
2021-01-22 23:03:17 +0200

    Update and rename zafdfz.py to zafddfz.py

--- zafddfz.py
+++ zafddfz.py
@@ -1,10 +1,8 @@
 from .. import loader, utils
 @loader.tds
 class TpMod(loader.Module):
-	"""ТП"""
-	strings = {"name": "t"}
+	strings = {"name": "Тех Поддержка"}
 	@loader.owner
 	async def tpcmd(self, message):
-		"""<text>"""
 		text = utils.get_args_raw(message)
 		await message.edit("👩🏻‍💻 Ответ от агента: Sofi Storub\n\n"+text+"\n\nС уважением, казино X2Casino!")
