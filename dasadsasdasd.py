b3715758d190fd5d4914fc8eccd5cfe0dae07e18
Morty <52600928+MortyLZT@users.noreply.github.com>
2021-01-22 23:08:32 +0200

    Update dasadsasdasd.py

--- dasadsasdasd.py
+++ dasadsasdasd.py
@@ -3,6 +3,6 @@ from .. import loader, utils
 class TpMod(loader.Module):
 	strings = {"name": "Тех Поддержка"}
 	@loader.owner
-	async def ""+cmd(self, message):
+	async def cmd(self, message):
 		text = utils.get_args_raw(message)
 		await message.edit("👩🏻‍💻 Ответ от агента: Sofi Storub\n\n"+text+"\n\nС уважением, казино X2Casino!")
